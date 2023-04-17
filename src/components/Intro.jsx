abab2dc2d8507da5b81f04862838088a6ab7b12b
SiddharthM05 <sidmalik.malik@gmail.com>
2023-04-17 21:01:25 +0530

    Added dark mode & Typing Animation

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,16 +1,30 @@
-import React from "react";
+import React,{useEffect,useState} from "react";
+import { TypeAnimation } from "react-type-animation";
 
 const Intro = () =>{
+    
     return (
         <div className="flex items-center justify-center flex-col
         text-center pt-20 pb-6">
-            <h1 className="text-4xl md:text-7xl mb-1
-            md:mb-3 font-bold">Siddharth</h1>
-            <p className="text-base md:text-xl mb-3 font-medium">Software Engineer</p>
-            <p className="text-sm max-w-xl mb-6 font-bold">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-            Vivamus eu pulvinar nibh, vitae scelerisque arcu. Nullam imperdiet, risus elementum tempor aliquam, mi nulla pharetra dui, a sollicitudin leo quam a turpis. Praesent ultricies metus felis, nec consectetur libero tincidunt ullamcorper. Nam quis quam in libero ullamcorper tincidunt. Quisque ultrices elit vel orci tempus, vel consequat mi auctor. Suspendisse felis ante, feugiat at ultricies vitae, laoreet in nisl. Sed sit amet diam nec dui ultrices laoreet. Ut mollis nibh arcu, quis tempus ligula imperdiet a. Nam ultricies eget metus quis condimentum. Curabitur ut nisl eget nunc tempus dictum. Proin risus nibh, tempus in purus vel, mollis pulvinar ex. Aliquam lacinia erat at purus venenatis imperdiet. Sed pellentesque metus est, accumsan semper magna auctor non. Suspendisse eu augue quis nisi eleifend suscipit sit amet vel mi. Fusce ultricies feugiat bibendum. Vestibulum mi mauris, dictum in mattis nec, tristique ut urna.
-            </p>
+            <h1 className="text-4xl dark:text-white md:text-7xl mb-1
+            md:mb-3 font-bold">Siddharth Malik</h1>
+            <span className="text-base md:text-xl mb-3 font-medium">
+  <span>I am a </span>{' '} 
+  <TypeAnimation
+    sequence={[
+      'Software Engineer',
+      1000,
+      'Backend Developer',
+      1000,
+      'Tech Enthusiast',
+      1000,
+    ]}
+    repeat={Infinity}
+  />
+</span>
+<p className=" text-base max-w-xl mb-6 font-bold">
+            Lorem Ipmsum
+         </p>
         </div>
     )
 }
