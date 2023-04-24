464576648c903bc3447a69f41611be2d9984878f
SiddharthM05 <sidmalik.malik@gmail.com>
2023-04-25 00:05:21 +0530

    Minor UI  changes

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,31 +1,40 @@
-import React,{useEffect,useState} from "react";
+import React, { useEffect, useState } from "react";
 import { TypeAnimation } from "react-type-animation";
+import { download } from "../data/svg";
 
-const Intro = () =>{
-    
-    return (
-        <div className="flex items-center justify-center flex-col
-        text-center pt-36 pb-10">
-            <h1 className="text-4xl dark:text-white md:text-7xl mb-1
-            md:mb-3 font-bold">Siddharth Malik</h1>
-            <span className="text-base md:text-xl mb-3 font-medium">
-  <span>I am a </span>{' '} 
-  <TypeAnimation
-    sequence={[
-      'Software Engineer',
-      1000,
-      'Backend Developer',
-      1000,
-      'Tech Enthusiast',
-      1000,
-    ]}
-    repeat={Infinity}
-  />
-</span>
-<p className=" text-base max-w-xl mb-6 font-bold">
-            Lorem Ipmsum
-         </p>
-        </div>
-    )
-}
+const Intro = () => {
+  return (
+    <div
+      className="flex items-center justify-center flex-col
+        text-center pt-36 pb-10"
+    >
+      <h1
+        className="text-4xl dark:text-white md:text-7xl mb-1
+            md:mb-3 font-bold"
+      >
+        Siddharth Malik
+      </h1>
+      <span className="text-base md:text-xl mb-3 font-medium">
+        <span>I am a </span>{" "}
+        <TypeAnimation
+          sequence={[
+            "Software Engineer",
+            1000,
+            "Backend Developer",
+            1000,
+            "Tech Enthusiast",
+            1000,
+          ]}
+          repeat={Infinity}
+        />
+      </span>
+      <p className=" text-base max-w-xl mb-6 font-bold">Lorem Ipmsum</p>
+
+      <button class=" p-2 z-10  bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center">
+        {download}
+        <span>Download CV</span>
+      </button>
+    </div>
+  );
+};
 export default Intro;
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -11,7 +11,7 @@ function Navbar({ projectsRef, timelineRef, contactRef }) {
     });
   };
   return (
-    <nav className=" z-20 hidden sm:block fixed w-full dark:bg-stone-900 border-b-2 border-gray-200 dark:border-gray-700 bg-white bg-opacity-95 dark:bg-opacity-95">
+    <nav className=" z-20  sm:block fixed w-full dark:bg-stone-900 border-b-2 border-gray-200 dark:border-gray-700 bg-white bg-opacity-95 dark:bg-opacity-95">
       <div ref={navBarRef} className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <DarkModeToggle></DarkModeToggle>
         <div className="flex justify-between h-16">
@@ -39,11 +39,6 @@ function Navbar({ projectsRef, timelineRef, contactRef }) {
               className="ml-4 dark:text-white font-medium"
             >
               Contact Me
-            </button>
-
-            <button class="fixed p-2 z-10 right-24 top-4 bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center">
-              {download}
-              <span>Download CV</span>
             </button>
           </div>
         </div>
